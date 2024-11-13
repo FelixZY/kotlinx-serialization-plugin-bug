1ee108fe29f1cf4a52f2b5fa3730e62b87e3af32
Felix Zedén Yverås <felix@fzy.se>
2024-11-13 23:35:49 +0100

    chore: create new project in AS

---
+++ app/src/androidTest/java/se/fzy/kotlinx_serialization_plugin_bug/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package se.fzy.kotlinx_serialization_plugin_bug
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("se.fzy.kotlinx_serialization_plugin_bug", appContext.packageName)
+    }
+}
